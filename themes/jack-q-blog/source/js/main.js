a72b4953510a2a944fe93a753051399c64a13dd2
Jack Q <QiaoBo@outlook.com>
2017-02-14 13:12:00 +0800

    responsive sidebar and nav header

--- themes/jack-q-blog/source/js/main.js
+++ themes/jack-q-blog/source/js/main.js
@@ -1,20 +1,29 @@
 ; +function ($) {
 
-  // aside toggle
+  // aside item toggle
   Array.prototype.forEach.call(
     $('.icon-item span'), function (i) {
-    i.addEventListener('click', function () {
-      var header = $('.aside-center').item(0);
-      if (header.classList.contains('active')) {
-        if(header.dataset['contentTab'] == i.dataset['content']){
-          header.classList.remove('active');
+      i.addEventListener('click', function () {
+        var header = $('.aside-center').item(0);
+        if (header.classList.contains('active')) {
+          if (header.dataset['contentTab'] == i.dataset['content']) {
+            header.classList.remove('active');
+          }
+        } else {
+          header.classList.add('active');
         }
-      } else {
-        header.classList.add('active');
-      }
-      header.dataset['contentTab'] = i.dataset['content'];
+        header.dataset['contentTab'] = i.dataset['content'];
+      });
     });
-  });
+
+  // aside block toggle  
+  Array.prototype.forEach.call($('.nav-bar-toggle'),
+    function (i) {
+      i.addEventListener('click', function () {
+        var aside = $('.jq-blog-aside').item(0);
+        aside.classList.toggle('toggle-on');
+      })
+    });
 
   // debugger;
 }(document.querySelectorAll.bind(document));
