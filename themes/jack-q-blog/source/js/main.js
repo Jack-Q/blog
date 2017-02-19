2dd23bcde4ec5335fb9fdf7cc293ed6c04d87a24
Jack Q <QiaoBo@outlook.com>
2017-02-19 23:01:51 +0800

    add smooth scroll

--- themes/jack-q-blog/source/js/main.js
+++ themes/jack-q-blog/source/js/main.js
@@ -1,10 +1,10 @@
-; +function ($) {
+; +function ($, sel) {
 
   // aside item toggle
   Array.prototype.forEach.call(
-    $('.icon-item span'), function (i) {
+    sel('.icon-item span'), function (i) {
       i.addEventListener('click', function () {
-        var header = $('.aside-center').item(0);
+        var header = sel('.aside-center').item(0);
         if (header.classList.contains('active')) {
           if (header.dataset['contentTab'] == i.dataset['content']) {
             header.classList.remove('active');
@@ -17,25 +17,34 @@
     });
 
   // aside block toggle  
-  Array.prototype.forEach.call($('.nav-bar-toggle'),
+  Array.prototype.forEach.call(sel('.nav-bar-toggle'),
     function (i) {
       i.addEventListener('click', function () {
-        var aside = $('.jq-blog-aside').item(0);
+        var aside = sel('.jq-blog-aside').item(0);
         aside.classList.toggle('toggle-on');
       })
     });
 
-  Array.prototype.forEach.call($('.toc .toc-link'), function (i) {
-    i.scrollIntoView &&
-      i.addEventListener('click', function (e) {
-        $(i.getAttribute('href')).item(0).scrollIntoView({
-          behavior: 'smooth'
-        })
-        // Close sidebar if necessary
-        $('.jq-blog-aside').item(0).classList.remove('toggle-on');
-        e.preventDefault();
-        return true;
-      });
+  Array.prototype.forEach.call(sel('.toc .toc-link'), function (i) {
+    i.addEventListener('click', function (e) {
+      // Native action 
+      // since the smooth scroll feature is only available on Firefox at this time, 
+      // this is disabled currently
+      // sel(i.getAttribute('href')).item(0).scrollIntoView({
+      //   behavior: 'smooth'
+      // })
+
+      // jQuery Fallback
+      var el = $(i.getAttribute('href'))
+      $('html, body').animate({
+        scrollTop: el.offset().top
+      }, 400);
+
+      // Close sidebar if necessary
+      sel('.jq-blog-aside').item(0).classList.remove('toggle-on');
+      e.preventDefault();
+      return true;
+    });
   });
   //debugger;
-}(document.querySelectorAll.bind(document));
+}(jQuery, document.querySelectorAll.bind(document));
